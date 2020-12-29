3d8ce8ad03ddde0ded9ffd233762c36f52765846
namcxn <namcx@namcx.info>
2020-12-30 00:39:54 +0700

    Add output

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "hostname_record" {
+  value  = [ 
+    for hostname, value in zipmap(values(cloudflare_record.records)[*].hostname, values(cloudflare_record.records)[*].value)
+     : map("hostname", hostname, "value", value)
+  ]
+}
+
